304b8570e352c1e68ca9dcc4e5d80a794711d159
Maximiliano Safran <maxizero@gmail.com>
2022-09-14 15:21:51 -0300

    Primer subida para Preentrega

---
+++ README.md
@@ -0,0 +1,1 @@
+# js-preentrega-1
---
+++ javascript/index.js
@@ -0,0 +1,220 @@
+// ARRAYS DE OBJETOS (Globales)
+let usuarios = []
+
+let productos = []
+
+/* ================ CLASE USUARIOS ================ */
+class Usuario {
+
+    // ATRIBUTOS
+    constructor(user, password, admin) {
+        this.user = user
+        this.password = password
+        this.admin = admin // true=admin false=cliente
+    }
+
+    // MÉTODOS
+    esAdmin = () => usuarios.find((usuario) => usuario.user === this.user).admin
+
+    concretarAltaUsuario = () => {
+        usuarios.push(this)
+    }
+}
+
+
+/* ================ CLASE PRODUCTO ================ */
+class Producto {
+
+    // ATRIBUTOS
+    constructor(tipoProd, marca, precio, stock) {
+        this.tipoProd = tipoProd
+        this.marca = marca
+        this.precio = precio
+        this.stock = stock
+    }
+
+    // MÉTODOS
+
+    comprar = (cant) => {
+        if (this.validarStock(cant)) {
+            this.disminuirStock(cant)
+            alert("Compra efectuada con éxito\nMuchas gracias!")
+            console.log(`Stock actualizado - Quedan ${this.stock} unidades del producto ${this.tipoProd} ${this.marca}`)
+        }
+        else {
+            alert(`Stock insuficiente - Hay ${this.stock} unidades de ${this.tipoProd} ${this.marca}`)
+        }
+    }
+
+    validarStock = (cant) => this.stock >= cant
+
+    disminuirStock = (cant) => {
+        this.stock = this.stock - cant
+    }
+
+    concretarAltaProducto = () => {
+        productos.push(this)
+    }
+}
+
+
+/* ================ DECLARACIÓN FUNCIONES ================ */
+
+
+function obtenerUsuarioPorTeclado() {
+    let usuario = prompt ("Ingrese su usuario: \n")
+    let password = prompt ("Ingrese contraseña: \n")
+    return new Usuario(usuario, password, false)
+}
+
+validarLogin = (userLogin, passLogin) => usuarios.some((usuario) => (usuario.user === userLogin && usuario.password === passLogin))
+
+usuarioExistente = (userAlta) => usuarios.some((usuario) => usuario.user === userAlta)
+
+function obtenerProductoPorTeclado() {
+    let tipoProd = prompt("Ingrese tipo de producto: ")
+    let marca = prompt("Ingrese marca del producto: ")
+    let precio = parseFloat(prompt("Ingrese precio del producto: "))
+    let cant = parseInt(prompt("Ingrese cantidad de unidades del producto: "))
+    return new Producto(tipoProd, marca, precio, cant)
+}
+
+function mostrarObjetoEnTexto(objeto) {
+    console.log("Producto\n---------")
+    for (clave in objeto) {
+        if (typeof objeto[clave] != "function") {
+            console.log(`${clave}:${objeto[clave]}`)
+        }
+    }
+}
+
+menuPrincipal = () => parseInt(prompt("Bienvenido, ingrese la opción deseada: \n\n1.  Login\n2.  Registrarme\n3.  Salir\n"))
+
+menuAdmin = (userLogin) => parseInt(prompt(`Bienvenido, ${userLogin} ingrese la opción deseada: \n\n1.  Alta Producto\n2.  Modificar Producto\n3.  Eliminar Producto\n4.  Salir\n`))
+
+menuCompra = (userLogin) => parseInt(prompt(`Bienvenido ${userLogin}, escoja el producto que desea comprar: \n\n` + mostrarCatalogoProductos()))
+
+function mostrarCatalogoProductos() {
+    let option = 1
+    let textoCatalogo = ""
+    productos.forEach((prod) => {textoCatalogo += `${option}.  ${prod.tipoProd}, ${prod.marca}, $${prod.precio}, ${prod.stock} unidades\n`; option++})
+    textoCatalogo += `${option}.  salir\n`
+    return textoCatalogo
+}
+
+function desplegarProcedimientoAdmin(userLogin) {
+    menuAdmin(userLogin)
+}
+
+function desplegarProcedimientoCompra(userLogin) {
+
+    let eleccionMenuCompra = 0
+    let eleccionMenuRepeat = 0
+    let salirMenuCompra = 0
+    let cantDeseadaProd = 0
+    let prodElegido = {}
+
+    while (true) { // itera hasta que el usuario salga o deje de comprar productos
+
+        salirMenuCompra = productos.length + 1
+
+        while (true) { // itera hasta que la opción ingresada sea válida
+            eleccionMenuCompra = menuCompra(userLogin)
+            if (eleccionMenuCompra > 0 && eleccionMenuCompra <= salirMenuCompra) {
+                break
+            }
+            alert("Opción incorrecta")
+        }
+
+        if (eleccionMenuCompra === salirMenuCompra) {
+            break
+        }
+        else {
+            prodElegido = productos[eleccionMenuCompra - 1]
+
+            while (true) { // itera hasta que la opción ingresada sea válida
+                cantDeseadaProd = parseInt(prompt(`Especifique la cantidad de unidades de ${prodElegido.tipoProd} ${prodElegido.marca} que desea comprar`))
+                if (cantDeseadaProd > 0) {
+                    break
+                }
+                alert("Opción incorrecta")
+            }
+
+            prodElegido.comprar(cantDeseadaProd)
+
+            while (true) { // itera hasta que la opción ingresada sea válida
+                eleccionMenuRepeat = parseInt(prompt("Desea realizar otra compra?\n1.  SI\n2.  NO"))
+                if ((eleccionMenuRepeat === 1 || eleccionMenuRepeat === 2)) {
+                    break
+                }
+                alert("Opción incorrecta")
+            }
+
+            if (eleccionMenuRepeat === 2) {
+                break
+            }
+        }
+    }
+}
+
+/* ================ DECLARACIÓN FUNCIÓN PRINCIPAL ================ */
+function main() {
+
+    let objectUser = {}
+
+    // CARGA DE CATÁLOGO
+    productos.push(new Producto("lapicera", "bic", 50, 10))
+    productos.push(new Producto("lapicera", "pelikan", 70, 5))
+    productos.push(new Producto("goma de borrar", "pelikan", 15, 15))
+    productos.push(new Producto("sacapuntas", "maped", 25, 4))
+
+    // GENERACIÓN DE USUARIOS
+    usuarios.push(new Usuario("admin", "1234", true))
+    usuarios.push(new Usuario("maxi", "maxizero", false))
+
+    while(true) {
+        let repeat = false
+        switch (menuPrincipal()) {
+            case 1:  // LOGIN
+                objectUser = obtenerUsuarioPorTeclado()
+                if (validarLogin(objectUser.user, objectUser.password)) {
+                    if (!objectUser.esAdmin()) {
+                        // MENÚ COMPRA PRODUCTOS PARA CLIENTES
+                        desplegarProcedimientoCompra(objectUser.user)
+                    }
+                    else {
+                        // MENÚ ADMINS
+                        desplegarProcedimientoAdmin(objectUser.user)
+                    }
+                }
+                else {
+                    alert("Login fallido - Usuario o contraseña incorrectos")
+                    repeat = true
+                }
+                break
+            case 2:  // REGISTRO
+                objectUser = obtenerUsuarioPorTeclado()
+                if (!usuarioExistente(objectUser.user)) {
+                    objectUser.concretarAltaUsuario()
+                    alert("Usuario registrado con éxito")
+                }
+                else {
+                    alert("Alta fallida - Usuario registrado previamente")
+                    repeat = true
+                }
+                break
+            case 3:  // SALIR
+                repeat = false
+                break
+            default:
+                alert("Opción incorrecta")
+        }
+        if (!repeat) {
+            break
+        }
+    }
+    alert("Muchas gracias por visitarnos, hasta luego!")
+}
+
+/* ================ LLAMADO FUNCIÓN PRINCIPAL ================ */
+main()
